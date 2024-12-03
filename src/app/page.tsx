517da6989ca8a438075ab11910e9b7752b09e84b
Chindanai Jitbunjong <chindanai.j@hyper-trade.com>
2024-12-03 19:58:16 +0700

    adjust main page and change to todo list

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,142 @@
-import Image from "next/image";
+"use client";
+
+import { useEffect, useState } from "react";
+import { itemListMockUpData } from "./mock-up-data";
+import { Item, TableItem } from "./types/item";
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [itemList, setItemList] = useState<Item[]>(itemListMockUpData);
+  const [itemListIdx, setItemListCount] = useState<number>(0);
+  const itemTableHeaders = Array.from(new Set(itemListMockUpData.map((data) => data.type)));
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  const [isTableClicked, setIsTableClicked] = useState<boolean>(false);
+  const [isTableButtonClicked, setIsTableButtonClicked] = useState<boolean>(false);
+  const [clickedItem, setClickedItem] = useState<TableItem>();
+
+  const [tableItemList, setTableItemList] = useState<TableItem[]>([]);
+
+  const onItemButtonClick = (item: Item) => {
+    setItemList((prvValues) => prvValues.filter((prvItem) => prvItem.name !== item.name));
+    setTableItemList((prvValues) => [
+      ...prvValues,
+      {
+        name: item.name,
+        type: item.type,
+        idx: itemListIdx,
+      },
+    ]);
+    setItemListCount((prvValue) => prvValue + 1);
+  };
+
+  useEffect(() => {
+    if (isTableClicked) {
+      onTableClick();
+      setIsTableClicked(false);
+    } else if (isTableButtonClicked) {
+      if (clickedItem) {
+        onTableButtonClick(clickedItem);
+        setIsTableButtonClicked(false);
+      }
+    }
+  }, [isTableClicked, isTableButtonClicked]);
+
+  const onClickTable = (event: React.MouseEvent<HTMLTableElement>) => {
+    const targetElement = event.target as HTMLElement;
+
+    if (targetElement.tagName === "TD") {
+      setIsTableClicked(true);
+      setIsTableButtonClicked(false);
+    }
+  };
+
+  const onClickButtonTable = (event: React.MouseEvent<HTMLButtonElement>, item: TableItem) => {
+    const targetElement = event.target as HTMLElement;
+
+    if (targetElement.tagName === "BUTTON") {
+      setIsTableButtonClicked(true);
+      setIsTableClicked(false);
+      setClickedItem(item);
+    }
+  };
+
+  const onTableClick = () => {
+    const combinedIdxList = tableItemList.map((item) => item.idx).sort((a, b) => a - b);
+    const firstSelectedIdx = combinedIdxList.shift();
+    const itemBySelectedIdx = tableItemList.find((item) => item.idx === firstSelectedIdx);
+
+    if (itemBySelectedIdx) {
+      setItemList((prvValues) => [...prvValues, itemBySelectedIdx]);
+      setTableItemList((prvValues) => prvValues.filter((prvItem) => prvItem.idx !== itemBySelectedIdx!.idx));
+    }
+  };
+
+  const onTableButtonClick = (item: TableItem) => {
+    setTableItemList((prvValues) => prvValues.filter((prvItem) => prvItem.idx !== item.idx));
+    setItemList((prvValues) => [...prvValues, item]);
+    setClickedItem(undefined);
+  };
+
+  return (
+    <div className="p-2 flex flex-row">
+      <div className="flex flex-col w-64 h-full">
+        {itemList.map((obj: Item, key) => {
+          return (
+            <button
+              onClick={() => onItemButtonClick(obj)}
+              key={key}
+              className="m-2 px-4 py-2 border-2 border-gray-300 bg-white hover:bg-blue-200 duration-150 text-black"
+            >
+              {obj.name}
+            </button>
+          );
+        })}
+      </div>
+      {itemTableHeaders.map((header) => {
+        return (
+          <div className="mx-2 py-2 w-64" key={header}>
+            <table
+              className="table-auto border-2 border-gray-300 h-full w-full"
+              onClick={(event) => onClickTable(event)}
+            >
+              <thead>
+                <tr>
+                  <th className="p-2 border-b-2 border-gray-300 bg-gray-200">
+                    <p className="text-gray-700">{header}</p>
+                  </th>
+                </tr>
+              </thead>
+              <tbody>
+                {tableItemList.filter((item) => item.type === header).length ? (
+                  <tr>
+                    <td className="align-top">
+                      {tableItemList
+                        .filter((item) => item.type === header)
+                        .map((item) => {
+                          return (
+                            <div className="p-2 w-full" key={`${item.name}-${item.idx}`}>
+                              <button
+                                className="w-full px-4 py-2 border-2 border-gray-300 bg-white hover:bg-blue-200 duration-150 text-black"
+                                onClick={(e) => onClickButtonTable(e, item)}
+                              >
+                                {item.name}
+                              </button>
+                            </div>
+                          );
+                        })}
+                    </td>
+                  </tr>
+                ) : (
+                  <tr>
+                    <td>
+                      <div className="p-2"></div>
+                    </td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        );
+      })}
     </div>
   );
 }
